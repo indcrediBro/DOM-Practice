42c9499b9331e5ed983d909a2541945d4d8ac774
indcrediBro <152566488+indcrediBro@users.noreply.github.com>
2024-01-13 20:34:00 +0530

    all tasks done

--- src/scripts.ts
+++ src/scripts.ts
@@ -11,6 +11,12 @@ const btn5 = document.querySelector('.btn5') as HTMLElement;
 const btn6 = document.querySelector('.btn6') as HTMLElement;
 const btn7 = document.querySelector('.btn7') as HTMLElement;
 
+const sqr1 = document.querySelector('.sqr1') as HTMLElement;
+const sqr5 = document.querySelector('.sqr5') as HTMLElement;
+
+const inputField = document.querySelector('.input') as HTMLInputElement;
+const textDisplay = document.querySelector('.text-display') as HTMLDivElement;
+
 // Functions ---------------------------------------------------------------------------------------
 function changeBackgroundColor(elementSelector: string, color: string): void {
   const targetElement = document.querySelector(elementSelector);
@@ -76,6 +82,39 @@ function lastButtonFunction() {
   changeBackgroundColor('body', '#000000');
 }
 
+function changeColorToRedOnHover() {
+  changeBackgroundColor('.sqr1', 'red');
+}
+function changeColorBack() {
+  changeBackgroundColor('.sqr1', '#1fc2ae');
+}
+
+let intervalTime: number | null = null;
+
+function startCountdown() {
+  let count = 0;
+  const numberDisplay = sqr5;
+
+  intervalTime = window.setInterval(() => {
+    if (count < 10) {
+      count += 1;
+      numberDisplay.textContent = count.toString();
+    } else {
+      clearInterval(intervalTime as number);
+    }
+  }, 1000);
+}
+
+function resetCountdown() {
+  const element = sqr5;
+  clearInterval(intervalTime as number);
+  element.textContent = '0';
+}
+
+function updateTextDisplay() {
+  const inputValue = inputField.value;
+  textDisplay.textContent = inputValue;
+}
 // Tasks Processes ---------------------------------------------------------------------------------
 btn1.addEventListener('click', changeFirstSquareColorToYellow);
 btn2.addEventListener('click', changeText);
@@ -84,3 +123,11 @@ btn4.addEventListener('click', changeFourthSquareTransparency);
 btn5.addEventListener('click', shuffleColorsOnFifthSquare);
 btn6.addEventListener('click', increaseNumber);
 btn7.addEventListener('click', lastButtonFunction);
+
+sqr1.addEventListener('mouseenter', changeColorToRedOnHover);
+sqr1.addEventListener('mouseleave', changeColorBack);
+
+sqr5.addEventListener('mouseover', startCountdown);
+sqr5.addEventListener('mouseleave', resetCountdown);
+
+inputField.addEventListener('input', updateTextDisplay);
